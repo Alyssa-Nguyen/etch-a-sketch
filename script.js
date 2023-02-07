687937055a18c86b6f7194a3f45160dbbfc54ac1
Alyssa Nguyen <nguyenm23@yahoo.com>
2023-02-06 17:53:47 -0800

    updates; reset grid, validate func, add color btns

--- script.js
+++ script.js
@@ -1,50 +1,100 @@
 
-
-
-// create a var to reference the container div
+// reference the container div
 const gridContainer = document.querySelector('.gridContainer');
 
-// create a var to reference the button for input
+// reference the 'Create a Grid" button for user input
 const gridPrompt = document.querySelector('.input');
 
-// when user click on the button, it will activiate event listener and prompt the user for a number
-// to make a grid, it will then call a function makeGrid() to make the grid
+const resetGrid = document.querySelector('.reset');
+
+
+
+
+resetGrid.addEventListener('click', () => {
+    removeAllChildNodes(gridContainer);
+    makeGrid(16);
+})
+
+// set the default grid to 16 square 
+function defaultGrid() {
+    makeGrid(16);
+}
+// generic remove child function, using parent node as an arg, this func will remove all childNodes ex: all divs within gridContainer
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+// click event listenener on 'Create a Grid' button to reset grid (remove all cells) & prompt user for grid size, then create grid based on user's input. 
 gridPrompt.addEventListener('click', () => {
-    let input = prompt("For the grid square, pls enter a number from 2-100?");
-    console.log(input);
-    const madeGrid = makeGrid(input);         //create a var to reference the makeGrid() function, also is calling this func
+    removeAllChildNodes(gridContainer);
+    let input = prompt("For the grid square, pls enter a number from 2-100?"); 
+    validateNum(input);       
 });
 
+
+// to validate the number for the grid size, cannot be over 100 or less than 2
+function validateNum(num) {    
+    if (num >= 2 && num <= 100) {
+        makeGrid(num);
+    } else {
+        do {
+             num = prompt("invalid, pick a number");
+        }
+        while (num < 2 || num > 100);
+        makeGrid(num);
+    }
+}
+
+
 // function to make a grid/box based on the user input of num (# of cell per side)
-function makeGrid (num) { 
+function makeGrid(num) { 
     for (let i = 0; i < (num*num); i++) {
         let div = document.createElement('div');       // create new div element
-        let squareLength = (700 - (num*2))/num;        //create var to get the wide & height for the new div
+        let squareLength = (700-(num*2))/num;        //create var to get the wide & height for the new div
         squareLength = squareLength+'px';             
         div.style.width = squareLength;
-        div.style.height = squareLength;
-        //div.style.backgroundColor = 'aliceblue';        //weirdly if there's no border or text content in the div, the div won't show bg color 
-        //div.style.border = '1px solid grey';
-        div.classList.add("divGrid");                   //adding a new class to the div         
+        div.style.height = squareLength;             
+        div.classList.add("divGrid");                   //adding a new class to the div  weirdly if there's no border or text content in the div, the div won't show bg color        
         gridContainer.appendChild(div);                 //appending the div inside of the container div
     }
-    //following doesn't work when it is placed outside of this makeGrid() function somehow.
+                                                             //following doesn't work when it is placed outside of this makeGrid() function somehow.
     const divHover = document.querySelectorAll('.divGrid');      //referencing the new div cells just created
-    divHover.forEach(item => {
-    item.addEventListener('mouseenter', (e) => {
-        changeColor(item);
-        
+    const hoverBlack = document.querySelector('#hoverBlack');
+    const hoverPink = document.querySelector('#hoverPink');
+    
+    hoverBlack.addEventListener('click', () => {
+        divHover.forEach(item => {
+            item.addEventListener('mouseenter', (e) => {
+                changeBlack(item);
+            });
+        });    
     });
-});
+
+    hoverPink.addEventListener('click', () => {
+        divHover.forEach(item => {
+            item.addEventListener('mouseenter', (e) => {
+                changePink(item);
+            });
+        });    
+    });   
 
 }
 
 
-const changeColor = (item) => {
+
+const changeBlack = (item) => {
     item.setAttribute('id', 'changeBlack');
-};
+}; 
+
+const changePink = (item) => {
+    item.setAttribute('id', 'changePink');
+}
 
 
+
+defaultGrid();
 
 /* 
 the following works even though it is out of makeGrid() function, not sure why yet, maybe 
@@ -71,6 +121,7 @@ makeDivs();
 
 // looping through the divs/boxes and changing the color by
 // hovering the mouse over them
+
 const divBoxes = document.querySelectorAll('.divBox');
 
 divBoxes.forEach(item => {
@@ -82,6 +133,7 @@ divBoxes.forEach(item => {
 
 // function to change color by adding id element
 // do I use document.createElement('')  ?? 
+
 const changeColor = (item) => {
     item.setAttribute('id', 'changeBlack');
 
